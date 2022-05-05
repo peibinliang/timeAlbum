086a26363ddc093350f782e06318d324836021ea
peibinliang <1805993643@qq.com>
2022-05-05 09:58:54 +0800

    初交

---
+++ src/test/java/com/time/album/timealbum/TimeAlbumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.time.album.timealbum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeAlbumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
